f09000cf4a9d528725f04a4a0b5013c552b6e8f4
Weylin <wprenison@gmail.com>
2013-10-14 23:51:14 +0200

    Visual Studio Project upload + frmAnimalDetails GUI

---
+++ iShelter/iShelter/frmAnimalDetails.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace iShelter
+{
+    partial class iShelter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnsAnimal = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abouToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpF1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtbName = new System.Windows.Forms.TextBox();
+            this.cmbSpecies = new System.Windows.Forms.ComboBox();
+            this.lblSpecies = new System.Windows.Forms.Label();
+            this.txtbBreed = new System.Windows.Forms.TextBox();
+            this.lblBreed = new System.Windows.Forms.Label();
+            this.txtbIllnesses = new System.Windows.Forms.TextBox();
+            this.lblIllnesses = new System.Windows.Forms.Label();
+            this.txtbInjuries = new System.Windows.Forms.TextBox();
+            this.lblInjuries = new System.Windows.Forms.Label();
+            this.txtbNotes = new System.Windows.Forms.TextBox();
+            this.lblNotes = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.lblRoomNo = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.btnTakePic = new System.Windows.Forms.Button();
+            this.grpbGender = new System.Windows.Forms.GroupBox();
+            this.rabM = new System.Windows.Forms.RadioButton();
+            this.rabF = new System.Windows.Forms.RadioButton();
+            this.btnProceed = new System.Windows.Forms.Button();
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsAnimal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.grpbGender.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnsAnimal
+            // 
+            this.mnsAnimal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.reportsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.mnsAnimal.Location = new System.Drawing.Point(0, 0);
+            this.mnsAnimal.Name = "mnsAnimal";
+            this.mnsAnimal.Size = new System.Drawing.Size(317, 24);
+            this.mnsAnimal.TabIndex = 0;
+            this.mnsAnimal.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abouToolStripMenuItem,
+            this.helpF1ToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.aboutToolStripMenuItem.Text = "Help";
+            // 
+            // abouToolStripMenuItem
+            // 
+            this.abouToolStripMenuItem.Name = "abouToolStripMenuItem";
+            this.abouToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.abouToolStripMenuItem.Text = "About";
+            // 
+            // helpF1ToolStripMenuItem
+            // 
+            this.helpF1ToolStripMenuItem.Name = "helpF1ToolStripMenuItem";
+            this.helpF1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.helpF1ToolStripMenuItem.Text = "Help F1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(213, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(82, 74);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(7, 43);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name:";
+            // 
+            // txtbName
+            // 
+            this.txtbName.Location = new System.Drawing.Point(61, 40);
+            this.txtbName.Name = "txtbName";
+            this.txtbName.Size = new System.Drawing.Size(121, 20);
+            this.txtbName.TabIndex = 3;
+            // 
+            // cmbSpecies
+            // 
+            this.cmbSpecies.FormattingEnabled = true;
+            this.cmbSpecies.Location = new System.Drawing.Point(61, 66);
+            this.cmbSpecies.Name = "cmbSpecies";
+            this.cmbSpecies.Size = new System.Drawing.Size(121, 21);
+            this.cmbSpecies.TabIndex = 4;
+            // 
+            // lblSpecies
+            // 
+            this.lblSpecies.AutoSize = true;
+            this.lblSpecies.Location = new System.Drawing.Point(7, 69);
+            this.lblSpecies.Name = "lblSpecies";
+            this.lblSpecies.Size = new System.Drawing.Size(48, 13);
+            this.lblSpecies.TabIndex = 5;
+            this.lblSpecies.Text = "Species:";
+            // 
+            // txtbBreed
+            // 
+            this.txtbBreed.Location = new System.Drawing.Point(61, 94);
+            this.txtbBreed.Name = "txtbBreed";
+            this.txtbBreed.Size = new System.Drawing.Size(121, 20);
+            this.txtbBreed.TabIndex = 6;
+            // 
+            // lblBreed
+            // 
+            this.lblBreed.AutoSize = true;
+            this.lblBreed.Location = new System.Drawing.Point(7, 97);
+            this.lblBreed.Name = "lblBreed";
+            this.lblBreed.Size = new System.Drawing.Size(38, 13);
+            this.lblBreed.TabIndex = 7;
+            this.lblBreed.Text = "Breed:";
+            // 
+            // txtbIllnesses
+            // 
+            this.txtbIllnesses.Location = new System.Drawing.Point(61, 121);
+            this.txtbIllnesses.Multiline = true;
+            this.txtbIllnesses.Name = "txtbIllnesses";
+            this.txtbIllnesses.Size = new System.Drawing.Size(121, 30);
+            this.txtbIllnesses.TabIndex = 8;
+            // 
+            // lblIllnesses
+            // 
+            this.lblIllnesses.AutoSize = true;
+            this.lblIllnesses.Location = new System.Drawing.Point(7, 124);
+            this.lblIllnesses.Name = "lblIllnesses";
+            this.lblIllnesses.Size = new System.Drawing.Size(50, 13);
+            this.lblIllnesses.TabIndex = 9;
+            this.lblIllnesses.Text = "Illnesses:";
+            // 
+            // txtbInjuries
+            // 
+            this.txtbInjuries.Location = new System.Drawing.Point(61, 157);
+            this.txtbInjuries.Multiline = true;
+            this.txtbInjuries.Name = "txtbInjuries";
+            this.txtbInjuries.Size = new System.Drawing.Size(121, 30);
+            this.txtbInjuries.TabIndex = 10;
+            // 
+            // lblInjuries
+            // 
+            this.lblInjuries.AutoSize = true;
+            this.lblInjuries.Location = new System.Drawing.Point(7, 160);
+            this.lblInjuries.Name = "lblInjuries";
+            this.lblInjuries.Size = new System.Drawing.Size(43, 13);
+            this.lblInjuries.TabIndex = 11;
+            this.lblInjuries.Text = "Injuries:";
+            // 
+            // txtbNotes
+            // 
+            this.txtbNotes.Location = new System.Drawing.Point(61, 193);
+            this.txtbNotes.Multiline = true;
+            this.txtbNotes.Name = "txtbNotes";
+            this.txtbNotes.Size = new System.Drawing.Size(121, 53);
+            this.txtbNotes.TabIndex = 12;
+            // 
+            // lblNotes
+            // 
+            this.lblNotes.AutoSize = true;
+            this.lblNotes.Location = new System.Drawing.Point(7, 196);
+            this.lblNotes.Name = "lblNotes";
+            this.lblNotes.Size = new System.Drawing.Size(38, 13);
+            this.lblNotes.TabIndex = 13;
+            this.lblNotes.Text = "Notes:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(250, 183);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(49, 20);
+            this.maskedTextBox1.TabIndex = 14;
+            // 
+            // lblRoomNo
+            // 
+            this.lblRoomNo.AutoSize = true;
+            this.lblRoomNo.Location = new System.Drawing.Point(189, 186);
+            this.lblRoomNo.Name = "lblRoomNo";
+            this.lblRoomNo.Size = new System.Drawing.Size(55, 13);
+            this.lblRoomNo.TabIndex = 15;
+            this.lblRoomNo.Text = "Room No:";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(250, 157);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(49, 20);
+            this.maskedTextBox2.TabIndex = 16;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(189, 160);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(29, 13);
+            this.lblAge.TabIndex = 17;
+            this.lblAge.Text = "Age:";
+            // 
+            // btnTakePic
+            // 
+            this.btnTakePic.Location = new System.Drawing.Point(209, 124);
+            this.btnTakePic.Name = "btnTakePic";
+            this.btnTakePic.Size = new System.Drawing.Size(90, 23);
+            this.btnTakePic.TabIndex = 18;
+            this.btnTakePic.Text = "Take Picture";
+            this.btnTakePic.UseVisualStyleBackColor = true;
+            // 
+            // grpbGender
+            // 
+            this.grpbGender.Controls.Add(this.rabF);
+            this.grpbGender.Controls.Add(this.rabM);
+            this.grpbGender.Location = new System.Drawing.Point(192, 216);
+            this.grpbGender.Name = "grpbGender";
+            this.grpbGender.Size = new System.Drawing.Size(107, 30);
+            this.grpbGender.TabIndex = 19;
+            this.grpbGender.TabStop = false;
+            this.grpbGender.Text = "Gender";
+            // 
+            // rabM
+            // 
+            this.rabM.AutoSize = true;
+            this.rabM.Location = new System.Drawing.Point(18, 12);
+            this.rabM.Name = "rabM";
+            this.rabM.Size = new System.Drawing.Size(34, 17);
+            this.rabM.TabIndex = 0;
+            this.rabM.TabStop = true;
+            this.rabM.Text = "M";
+            this.rabM.UseVisualStyleBackColor = true;
+            // 
+            // rabF
+            // 
+            this.rabF.AutoSize = true;
+            this.rabF.Location = new System.Drawing.Point(58, 11);
+            this.rabF.Name = "rabF";
+            this.rabF.Size = new System.Drawing.Size(31, 17);
+            this.rabF.TabIndex = 1;
+            this.rabF.TabStop = true;
+            this.rabF.Text = "F";
+            this.rabF.UseVisualStyleBackColor = true;
+            // 
+            // btnProceed
+            // 
+            this.btnProceed.Location = new System.Drawing.Point(209, 252);
+            this.btnProceed.Name = "btnProceed";
+            this.btnProceed.Size = new System.Drawing.Size(90, 23);
+            this.btnProceed.TabIndex = 20;
+            this.btnProceed.Text = "Proceed";
+            this.btnProceed.UseVisualStyleBackColor = true;
+            // 
+            // reportsToolStripMenuItem
+            // 
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.reportsToolStripMenuItem.Text = "Reports";
+            // 
+            // iShelter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 283);
+            this.Controls.Add(this.btnProceed);
+            this.Controls.Add(this.grpbGender);
+            this.Controls.Add(this.btnTakePic);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.lblRoomNo);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.lblNotes);
+            this.Controls.Add(this.txtbNotes);
+            this.Controls.Add(this.lblInjuries);
+            this.Controls.Add(this.txtbInjuries);
+            this.Controls.Add(this.lblIllnesses);
+            this.Controls.Add(this.txtbIllnesses);
+            this.Controls.Add(this.lblBreed);
+            this.Controls.Add(this.txtbBreed);
+            this.Controls.Add(this.lblSpecies);
+            this.Controls.Add(this.cmbSpecies);
+            this.Controls.Add(this.txtbName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.mnsAnimal);
+            this.MainMenuStrip = this.mnsAnimal;
+            this.Name = "iShelter";
+            this.Text = "iShelter";
+            this.mnsAnimal.ResumeLayout(false);
+            this.mnsAnimal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.grpbGender.ResumeLayout(false);
+            this.grpbGender.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnsAnimal;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abouToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpF1ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtbName;
+        private System.Windows.Forms.ComboBox cmbSpecies;
+        private System.Windows.Forms.Label lblSpecies;
+        private System.Windows.Forms.TextBox txtbBreed;
+        private System.Windows.Forms.Label lblBreed;
+        private System.Windows.Forms.TextBox txtbIllnesses;
+        private System.Windows.Forms.Label lblIllnesses;
+        private System.Windows.Forms.TextBox txtbInjuries;
+        private System.Windows.Forms.Label lblInjuries;
+        private System.Windows.Forms.TextBox txtbNotes;
+        private System.Windows.Forms.Label lblNotes;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label lblRoomNo;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Button btnTakePic;
+        private System.Windows.Forms.GroupBox grpbGender;
+        private System.Windows.Forms.RadioButton rabF;
+        private System.Windows.Forms.RadioButton rabM;
+        private System.Windows.Forms.Button btnProceed;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+    }
+}
+
